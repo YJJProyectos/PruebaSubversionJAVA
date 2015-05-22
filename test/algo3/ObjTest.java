03894e605195070f4e8de8a92f525b11ae4517ff
Yang <YJJProyectos@users.noreply.github.com>
2015-05-22 13:48:27 -0300

    aplique cambios

--- test/algo3/ObjTest.java
+++ test/algo3/ObjTest.java
@@ -8,9 +8,17 @@ public class ObjTest {
 	@Test
 	public void testeando() {
 		
-	
+	    Obj obj = new Obj();
+		obj.hacer();
 		Assert.assertEquals("hola", obj.mostrar());
-	}
-
+	   }
+    @Test
+    public void testing(){
+    	
+    	Obj obj = new Obj();
+		obj.hacer();
+		Assert.assertEquals("hola", obj.mostrar());
+    	
+    }
 }
 // Modifico para probar
