d05fba1368c3cbdd99fea3776da351650d1db804
Yang <YJJProyectos@users.noreply.github.com>
2015-05-22 13:01:44 -0300

    Agrege test Obj

---
+++ src/algo3/Obj.java
@@ -0,0 +1,14 @@
+package algo3;
+
+public class Obj {
+
+	public void hacer() {
+		
+	}
+
+	public String mostrar() {
+
+		return "hola";
+	}
+
+}
---
+++ test/algo3/ObjTest.java
@@ -0,0 +1,16 @@
+package algo3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ObjTest {
+
+	@Test
+	public void testeando() {
+		
+		Obj obj = new Obj();
+		obj.hacer();
+		Assert.assertEquals("hola", obj.mostrar());
+	}
+
+}
